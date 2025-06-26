da42983693db61fe70dbfddd606466414265c25b
silalahi06 <54274062+silalahi06@users.noreply.github.com>
2025-06-26 10:36:40 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,6 @@
+// This acts like our initial JSON data
+const jsonData = [
+  { name: "Alice", jabatan: "Driver", email: "alice@example.com" },
+  
+  { name: "Bob", jabatan: "Driver", email: "bob@example.com" }
+];
